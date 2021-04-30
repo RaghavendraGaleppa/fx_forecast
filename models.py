9a242a8d7fc7c018d6a26ac2927a2efd32e3865e
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2021-04-30 19:56:22 +0530

    FME

--- models.py
+++ models.py
@@ -62,7 +62,7 @@ class BasicLinearModel(pl.LightningModule):
         x, y = train_batch
         logits = self.forward(x)
         loss = self.mse_loss(logits, y)
-        accuracy = self.accuracy_metric(x, logits, loss)
+        accuracy = self.accuracy_metric(x.detach().cpu().clone(), logits, loss)
         self.log('train_loss', loss)
         self.log('accuracy_score', accuracy)
         return loss, accuracy
@@ -71,7 +71,7 @@ class BasicLinearModel(pl.LightningModule):
         x, y = val_batch
         logits = self.forward(x)
         loss = self.mse_loss(logits, y)
-        accuracy = self.accuracy_metric(x, logits, loss)
+        accuracy = self.accuracy_metric(x.detach().cpu().clone(), logits, loss)
         self.log('val_loss', loss)
         self.log('accuracy_score', accuracy)
         return loss, accuracy
