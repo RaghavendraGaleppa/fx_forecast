7b63a855c88efc128227015fbff807e2cd55e870
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2021-04-30 19:49:01 +0530

    FME

--- models.py
+++ models.py
@@ -19,6 +19,7 @@ class BasicLinearModel(pl.LightningModule):
             f"Epoch {self.trainer.current_epoch} training loss={self.trainer.progress_bar_dict['loss']}")
 
     def validation_epoch_end(self, outputs):
+        print(outputs)
         loss = outputs[0]
         loss = torch.stack(loss).mean()
         accuracy = outputs[1]
