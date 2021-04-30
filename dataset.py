4e75744fe673e3927343532fb50dacf9451ca6ad
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2021-04-30 17:39:01 +0530

    Pushed filenames to __init__ instead of prepare data

--- dataset.py
+++ dataset.py
@@ -3,14 +3,15 @@ from . import tools
 
 class ForexDataModule(pl.LightningDataModule):
 
-    def __init__(self, window_size, batch_size, label_size):
+    def __init__(self, window_size, batch_size, label_size, filenames):
         self.window_size = window_size
         self.batch_size = batch_size
         self.label_size = label_size
+        self.filenames = filenames
 
-    def prepare_data(self, filenames=[]):
+    def prepare_data(self):
         price_data, price_labels = tools.build_dataset(
-            *filenames,
+            *self.filenames,
             window_size=self.window_size,
             batch_size=self.batch_size,
             label_size=self.label_size
