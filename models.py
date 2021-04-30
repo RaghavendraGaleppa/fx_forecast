c4c6311916a4eb7f3318526dd4de6d50401e1d9e
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2021-04-30 20:01:48 +0530

    FME

--- models.py
+++ models.py
@@ -20,19 +20,20 @@ class BasicLinearModel(pl.LightningModule):
             f"Epoch {self.trainer.current_epoch} training loss={self.trainer.progress_bar_dict['loss']}")
 
     def validation_epoch_end(self, outputs):
-        loss_scores = [l[0] for l in outputs]
-        accuracy_scores = [o[1] for o in outputs]
+        #loss_scores = [l[0] for l in outputs]
+        #accuracy_scores = [o[1] for o in outputs]
 
-        print(f"Loss Scores: {loss_scores}")
-        print(f"Accuracy Scores: {accuracy_scores}")
+        #print(f"Loss Scores: {loss_scores}")
+        #print(f"Accuracy Scores: {accuracy_scores}")
 
-        loss = torch.stack(loss_scores).mean()
-        accuracy = np.mean(accuracy_scores)
+        loss = torch.stack(outputs).mean()
+        #accuracy = np.mean(accuracy_scores)
 
         self.trainer.progress_bar_callback.main_progress_bar.write(
             f"Epoch {self.trainer.current_epoch} "
             f"validation_loss={loss.item()} "
-            f"validation_accuracy={accuracy}")
+            #f"validation_accuracy={accuracy}"
+        )
 
     def forward(self, x):
         batch_size, window_size, features = x.size()
